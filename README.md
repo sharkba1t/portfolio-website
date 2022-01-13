c97da59de043f8347331a73f47e8c0d5aad7b7d2
sharkba1t <87448201+sharkba1t@users.noreply.github.com>
2022-01-12 20:59:21 -0500

    finish site

---
+++ README.md
@@ -0,0 +1,6 @@
+Photo by <a href="https://unsplash.com/@florianolv?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Florian Olivo</a> on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@cdr6934?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Chris Ried</a> on <a href="https://unsplash.com/s/photos/python?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
+Photo by <a href="https://unsplash.com/@halacious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Hal Gatewood</a> on <a href="https://unsplash.com/s/photos/website?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
--- resources/js/main.js
+++ resources/js/main.js
@@ -1,28 +1,19 @@
 const mainDisplay = document.querySelector('.mainDisplay');
+const defaultText = document.getElementById('default-text');
 const isTabSelected = false;
 let currentTab;
+let currentProject;
 var previousTab;
+var previousProject
 function showProject(tab) {
-    currentTab = document.querySelector(`.tab${tab}`);
-    switch(tab){
-        case 1:
-            mainDisplay.innerHTML = "<p>test</p>";
+    if (document.querySelector(`.tab${tab}`) !== previousTab){
+            defaultText.style.display = 'none';
+            currentTab = document.querySelector(`.tab${tab}`);
+            currentProject = document.querySelector(`.project${tab}`);
+            currentProject.style.display = 'block';
             isSelected(currentTab);
             previousTab = currentTab;
-            break;
-        case 2:
-            mainDisplay.innerHTML = "<p>test 2</p>";
-            isSelected(currentTab);
-            previousTab = currentTab;
-            break;
-        case 3:
-            mainDisplay.innerHTML = "<p>test 3</p>";
-            isSelected(currentTab);
-            previousTab = currentTab;
-            break;
-        default:
-            mainDisplay.innerHTML = "<p>Something has gone wrong</p>";
-            
+            previousProject = currentProject;
     }
 }
 
@@ -30,5 +21,12 @@ function isSelected(tab, previous) {
         tab.className += ' tab-highlight';
         if (previousTab){
             previousTab.classList.remove('tab-highlight');
+            previousProject.style.display = 'none';
         }
     }
+
+const mobileNavButton = document.querySelector(".mobile-burger");
+const mobileNavBar = document.querySelector('.mobileNav');
+mobileNavButton.addEventListener('click', ()=> {
+mobileNavBar.style.display === 'block' ? mobileNavBar.style.display = 'none' : mobileNavBar.style.display = 'block';
+})
